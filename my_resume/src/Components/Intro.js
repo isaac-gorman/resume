b0a58f35952629c4b340b30916ace28b23408f8a
Isaac Gorman <isaacgorman@Isaacs-MBP.lan>
2021-01-12 15:01:51 -0600

    prog3 svg on hover change

--- my_resume/src/Components/Intro.js
+++ my_resume/src/Components/Intro.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import styled from "styled-components";
+import Git from './Assets/Git.svg'
 import './Intro.css';
+
+const Name = styled.p`
+    font-family: 'medium';
+    font-weight: normal;
+    font-size: 24px;
+`;
+
+const Occupation = styled.p`
+    font-family: 'medium';
+    font-weight: normal;
+    font-size: 14px;
+    color: #828282;
+`;
+
+const GitHubIcon = styled.img`
+  width: 100px;
+  height: 100px;
+  &:hover {
+    filter: invert(0.4);
+  }
+`;
 
 function Intro() {
     return(
         <div>
-            <p className="Name">Isaac Gorman</p>
-            <p className="Occupation">Designer | Studnet</p>
+            <Name>Isaac Gorman</Name>
+            <Occupation>Designer | Studnet</Occupation>
+            {/* I need to add the three svg links */}
+            <GitHubIcon src={Git} alt="nan" /> 
         </div>
     )
 }
