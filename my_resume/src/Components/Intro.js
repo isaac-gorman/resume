9350d99571c2b1a396168ea9d4d221097aafee36
Isaac Gorman <isaacgorman@Isaacs-MBP.lan>
2021-01-12 15:15:09 -0600

    prog4

--- my_resume/src/Components/Intro.js
+++ my_resume/src/Components/Intro.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from "styled-components";
 import Git from './Assets/Git.svg'
+import Dribbble from './Assets/Dribbble.svg'
+import Linkedin from './Assets/Linkedin.svg'
 import './Intro.css';
 
 const Name = styled.p`
@@ -17,8 +19,22 @@ const Occupation = styled.p`
 `;
 
 const GitHubIcon = styled.img`
-  width: 100px;
-  height: 100px;
+  width: 40px;
+  height: 40px;
+  &:hover {
+    filter: invert(0.4);
+  }
+`;
+const LinkedinIcon = styled.img`
+  width: 40px;
+  height: 40px;
+  &:hover {
+    filter: invert(0.4);
+  }
+`;
+const DribbbleIcon = styled.img`
+  width: 40px;
+  height: 40px;
   &:hover {
     filter: invert(0.4);
   }
@@ -31,6 +47,8 @@ function Intro() {
             <Occupation>Designer | Studnet</Occupation>
             {/* I need to add the three svg links */}
             <GitHubIcon src={Git} alt="nan" /> 
+            <LinkedinIcon src={Linkedin} alt="nan" /> 
+            <DribbbleIcon src={Dribbble} alt="nan" /> 
         </div>
     )
 }
